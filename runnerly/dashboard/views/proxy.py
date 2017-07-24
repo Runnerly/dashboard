a2fc3aed2b81346f05217a82ba6a4281a65a37b1
Tarek Ziade <tarek@ziade.org>
2017-07-24 12:57:40 +0200

    reorganized

---
+++ runnerly/dashboard/views/proxy.py
@@ -0,0 +1,61 @@
+import requests
+from flask import Blueprint, abort, session, jsonify
+from flask import current_app as app
+
+
+proxy = Blueprint('proxy', __name__)
+
+
+def email_to_uid(email):
+    return 1
+
+
+
+def get_token():
+    data = [('client_id', app.config['TOKENDEALER_CLIENT_ID']),
+        ('client_secret', app.config['TOKENDEALER_CLIENT_SECRET']),
+        ('audience', 'runnerly.io'),
+        ('grant_type', 'client_credentials')]
+
+
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    url = app.config['TOKENDEALER'] + '/oauth/token'
+    resp = requests.post(url, data=data, headers=headers)
+    return resp.json()['access_token']
+
+
+_TOKEN = 'blak'
+
+
+def get_auth_header(new=False):
+    global _TOKEN
+    if _TOKEN is None or new:
+        _TOKEN = get_token()
+    return 'Bearer ' + _TOKEN
+
+
+def call_data_service(endpoint):
+    attempt = 1
+    token = get_auth_header()
+    while attempt < 3:
+        resp = requests.get(app.config['DATASERVICE'] + endpoint,
+                            headers={'Authorization': token})
+        if not resp.status_code == 401:
+            break
+
+        # the token might be revoked
+        token = get_auth_header(new=True)
+        attempt += 1
+    return resp
+
+
+@proxy.route('/api/runs/<int:year>/<int:month>')
+def runs(year, month):
+    if 'user' not in session:
+        abort(401)
+    uid = email_to_uid(session['user'])
+    endpoint = '/runs/%d/%d/%d' % (uid, year, month)
+    resp = call_data_service(endpoint)
+    return jsonify(resp.json())
+
+
